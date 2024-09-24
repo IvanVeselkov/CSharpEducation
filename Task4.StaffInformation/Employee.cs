f3bf8f29a0700f9fedd14e0b483326df2f0b1d0c
IvanVeselkov <32011610+IvanVeselkov@users.noreply.github.com>
2024-09-24 21:11:12 +0400

    StaffInformation
    Консольное приложение, которое позволит управлять данными сотрудников.
    Программа обеспечивает функционал добавления, обновления,
    получения информации о работниках и расчета их зарплаты.

---
+++ Task4.StaffInformation/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace Task4.StaffInformation
+{
+  internal class Employee:IEmployee, IComparable<Employee>
+  {
+    public string FullName { get; set; }
+    public double Salary { get => this.SalaryPerHour * this.WorkingHours * 4; }
+
+    public double SalaryPerHour { get; set; }
+    
+    public int WorkingHours { get; set; }
+    public virtual double CalculateBonus => Salary / 10;
+
+    public int CompareTo(Employee other)
+    {
+      if(other.FullName!=null)
+      {
+        return this.FullName.CompareTo(other.FullName);
+      }
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Конструктор класса сотрудник.
+    /// </summary>
+    /// <param name="name">Полное имя сотрудника (ФИО).</param>
+    /// <param name="salaryPerHour">Оплата сотрудника за час работы.</param>
+    /// <param name="workingHours">Недельная ставка (в часах).</param>
+    public Employee(string name,double salaryPerHour,int workingHours)
+    {
+      FullName = name;
+      SalaryPerHour = salaryPerHour;
+      WorkingHours = workingHours;
+    }
+
+    
+  }
+}
